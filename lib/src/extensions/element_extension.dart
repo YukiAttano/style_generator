436cf86f282d2f183aba788981240256945bd766
YukiAttano <2906.alexander@gmail.com>
2025-10-20 11:22:30 +0200

    remove unused function

--- lib/src/extensions/element_extension.dart
+++ lib/src/extensions/element_extension.dart
@@ -3,14 +3,7 @@
 import 'package:analyzer/dart/element/element.dart';
 
 extension ElementExtension on ElementAnnotation {
-
   bool isOfType(Element classType) {
     return computeConstantValue()?.type?.element == classType;
-  }
-
-  bool isOfTypeFromString(LibraryElement lib, String annotationClazz) {
-    Element? clazzElement = lib.exportNamespace.get2(annotationClazz);
-
-    return computeConstantValue()?.type?.element == clazzElement;
   }
 }
