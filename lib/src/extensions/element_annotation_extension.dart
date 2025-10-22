576a2cd27b8e8a113175d4e8e2a015f08fdbbb30
YukiAttano <2906.alexander@gmail.com>
2025-10-22 14:26:08 +0200

    move into subfolder

--- lib/src/extensions/element_annotation_extension.dart
+++
@@ -1,7 +0,0 @@
-import "package:analyzer/dart/element/element.dart";
-
-extension ElementAnnotationExtension on ElementAnnotation {
-  bool isOfType(Element classType) {
-    return computeConstantValue()?.type?.element == classType;
-  }
-}
--- lib/style_generator.dart
+++
@@ -1,9 +0,0 @@
-import "package:build/build.dart";
-
-import "src/style_builder/style_builder.dart";
-
-export "src/annotations/style.dart";
-export "src/annotations/style_key.dart";
-export "src/style_builder/style_builder.dart";
-
-Builder styleBuilder(BuilderOptions options) => StyleBuilder();
