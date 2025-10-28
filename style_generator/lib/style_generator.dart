72bd686ed162ba9fbd152c02847701303dece3d2
YukiAttano <2906.alexander@gmail.com>
2025-10-28 08:02:27 +0100

    add lerpDouble

--- style_generator/lib/style_generator.dart
+++ style_generator/lib/style_generator.dart
@@ -2,6 +2,8 @@ import "package:build/build.dart";
 
 import "src/style_builder/style_builder.dart";
 
+export "dart:ui" show lerpDouble;
+
 export "src/style_builder/style_builder.dart";
 
 Builder styleBuilder(BuilderOptions options) => StyleBuilder(options: options);
