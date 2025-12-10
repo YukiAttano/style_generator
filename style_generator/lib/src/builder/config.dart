ca10666d6cc916fb7f2bbc41720676c6ed07f361
YukiAttano <2906.alexander@gmail.com>
2025-12-10 14:51:13 +0100

    documentation

--- style_generator/lib/src/builder/config.dart
+++ style_generator/lib/src/builder/config.dart
@@ -1,9 +1,16 @@
+/// @docImport "../annotations/copy_with_config.dart";
+/// @docImport "../annotations/style_config.dart";
+library;
 
-
+/// Allows other classes to depend on 'C' as a Config with 'A' as annotation like [StyleConfig] and [CopyWithConfig]
+// ignore: one_member_abstracts .
 abstract class _BaseConfig<C extends _BaseConfig<C, A>, A> {
   _BaseConfig apply(A config);
 }
 
+// Simplify _BaseConfig
+/// Allows other classes to depend on a 'Config' with 'A' as annotation like [StyleConfig] and [CopyWithConfig]
 abstract interface class Config<A> extends _BaseConfig<Config<A>, A> {
+  @override
   Config<A> apply(A config);
 }
