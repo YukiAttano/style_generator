02ba5aebfc8f8a7787c2db9ad3de760e4b83cbb1
YukiAttano <2906.alexander@gmail.com>
2025-12-13 18:33:53 +0100

    support redirecting constructor

--- style_generator/lib/src/extensions/constructor_declaration_extension.dart
+++ style_generator/lib/src/extensions/constructor_declaration_extension.dart
@@ -1,6 +1,8 @@
 import "package:analyzer/dart/ast/ast.dart";
 import "package:analyzer/dart/element/element.dart";
 
+typedef ParameterLookup = String Function(FormalParameterElement element);
+
 extension ConstructorDeclarationExtension on ConstructorDeclaration {
   /// maps the constructor initializer parameter names to their corresponding field names
   ///
@@ -23,7 +25,7 @@ extension ConstructorDeclarationExtension on ConstructorDeclaration {
   /// Since one part of the initializers is for class-local field initialization and the other one for super-class parameters,
   /// the [lookup] method is required to look trough the structure of a super constructor to find the correct field.
   Map<String, String> mapParameterToField({
-    required String Function(FormalParameterElement element) lookup,
+    required ParameterLookup lookup,
   }) {
     Map<String, String> map = {};
 
@@ -43,35 +45,41 @@ extension ConstructorDeclarationExtension on ConstructorDeclaration {
           if (expr is SimpleIdentifier) {
             map[expr.name] = fieldName;
           }
-        // Those who are assigned to the super class
+        // Those who are redirecting to the super class
         // Example: `super(lastname: last, birthday: birth ?? DateTime.now());`
         case SuperConstructorInvocation():
-          for (var argument in i.argumentList.arguments) {
-            Expression? expr;
-
-            switch (argument) {
-              case SimpleIdentifier():
-                expr = argument;
-              case NamedExpression():
-                expr = argument.expression;
-            }
-
-            if (expr is BinaryExpression) expr = expr.leftOperand;
-
-            switch (expr) {
-              case SimpleIdentifier():
-                map[expr.name] = lookup(argument.correspondingParameter!);
-              //argument.correspondingParameter!.displayName;
-            }
-          }
+          _handleArguments(map, i.argumentList.arguments, lookup);
+        // Those who are redirecting to another constructor
+        // Example: `this._private(lastname: last, birthday: birth ?? DateTime.now());`
+        case RedirectingConstructorInvocation():
+          _handleArguments(map, i.argumentList.arguments, lookup);
 
         case AssertInitializer():
-          break;
-        case RedirectingConstructorInvocation():
           break;
       }
     }
 
     return map;
+  }
+
+  void _handleArguments(Map<String, String> map, NodeList<Expression> arguments, ParameterLookup lookup) {
+    for (var argument in arguments) {
+      Expression? expr;
+
+      switch (argument) {
+        case SimpleIdentifier():
+          expr = argument;
+        case NamedExpression():
+          expr = argument.expression;
+      }
+
+      if (expr is BinaryExpression) expr = expr.leftOperand;
+
+      switch (expr) {
+        case SimpleIdentifier():
+          map[expr.name] = lookup(argument.correspondingParameter!);
+        //argument.correspondingParameter!.displayName;
+      }
+    }
   }
 }
